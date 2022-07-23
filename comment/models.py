8196e52ad0cec7d53769f49ef86f8a1d4987f6ef
SMin1620 <ibb09@naver.com>
2022-07-23 23:58:00 +0900

    Feat - 댓글, 대댓글 모델 생성

---
+++ comment/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+from user.models import User
+from article.models import Article
+
+
+# Create your models here.
+class Comment(models.Model):
+    content = models.TextField('댓글 내용')
+
+    created_at = models.DateTimeField('생성 날짜', auto_now_add=True)
+    updated_at = models.DateTimeField('수정 날짜', auto_now=True)
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True)
+
+    class Meta:
+        db_table = 'comment'
+        ordering = ['-id']
+        verbose_name = '댓글'
+        verbose_name_plural = '댓글들'
+
+    def __str__(self):
+        return f'{self.id} // Article : {self.article}, User : {self.user}'
+
+
+class ReComment(models.Model):
+    content = models.TextField('대댓글 내용')
+
+    created_at = models.DateTimeField('생성 날짜', auto_now_add=True)
+    updated_at = models.DateTimeField('수정 날짜', auto_now=True)
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True)
+    comment = models.ForeignKey(Comment, on_delete=models.DO_NOTHING, blank=True)
+
+    class Meta:
+        db_table = 'comment'
+        ordering = ['-id']
+        verbose_name = '대댓글'
+        verbose_name_plural = '대댓글들'
+
+    def __str__(self):
+        return f'{self.id} // Article : {self.article}, User : {self.user} // Comment : {self.comment}'
+
+
+
+
